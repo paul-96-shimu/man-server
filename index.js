5a11732d31bd983c9855032b86510d68d3515f95
paul-96-shimu <shimupaul96@gmail.com>
2025-10-05 12:02:19 +0600

    added all api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT|| 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // password-0JGiJW5ep5YTH1SM
@@ -34,13 +34,73 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+
+const mansCollection= client.db('manstyle').collection('manproducts')
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+// man api
+
+  app.get('/manproducts', async (req, res) => {
+  
+      const products = mansCollection.find(); 
+      const result = await  products.toArray();
+      res.send(result);
+   
+  });
+
+
+  app.get('/manproducts/:id',async (req, res) =>{
+    const id =req.params.id;
+    const query ={_id: new ObjectId(id)};
+    const product =await mansCollection.findOne(query)
+    res.send(product)
+  })
+
+
+  app.get('/manproducts/:id/related', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const product = await mansCollection.findOne(query);
+
+    if (!product) {
+      return res.status(404).send({ error: 'Product not found' });
+    }
+
+    // একই category এর product, কিন্তু নিজেকে বাদ দিয়ে
+    const related = await mansCollection
+      .find({ 
+        category: product.category, 
+        _id: { $ne: new ObjectId(id) } 
+      })
+      .limit(4) // চাইলে related limit করতে পারো
+      .toArray();
+
+    res.send(related);
+  })
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
+
+
+
+  
+
+
+
+
+
+
 }
 run().catch(console.dir);
 
